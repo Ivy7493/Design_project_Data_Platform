c2b64be7ffc739d1b179305d9f1ca68aaa83d131
Ivan Von Staden <ivanblizz23@gmail.com>
2022-07-28 16:34:59 +0200

    Fixed Herokumail bug

--- scripts/configs/mailerConfig.js
+++ scripts/configs/mailerConfig.js
@@ -17,18 +17,28 @@ module.exports.sendConfirmationEmail = (name, email, confirmationCode) => {
     let port = process.env.PORT || 3000
     let addr = ""
     if(port == process.env.PORT){
-        addr = 'city-energy.herokuapp'
+        addr = 'city-energy.herokuapp.com'
+        transport.sendMail({
+            from: user,
+            to: email,
+            subject: "Please confirm your account",
+            html: `<h1>Email Confirmation</h1>
+                <h2>Hello ${name}</h2>
+                <p>Thank you for registering with city energy. Please confirm your email by clicking on the following link</p>
+                <a href=https://${addr}/confirm/?confirmationCode=${confirmationCode}>Confirm Account</a>
+                </div>`,
+          }).catch(err => console.log(err));
     }else{
         addr = '127.0.0.1'
-    }
-    transport.sendMail({
-      from: user,
-      to: email,
-      subject: "Please confirm your account",
-      html: `<h1>Email Confirmation</h1>
-          <h2>Hello ${name}</h2>
-          <p>Thank you for registering with city energy. Please confirm your email by clicking on the following link</p>
-          <a href=http://${addr}:${port}/confirm/?confirmationCode=${confirmationCode}>Confirm Account</a>
-          </div>`,
-    }).catch(err => console.log(err));
+        transport.sendMail({
+            from: user,
+            to: email,
+            subject: "Please confirm your account",
+            html: `<h1>Email Confirmation</h1>
+                <h2>Hello ${name}</h2>
+                <p>Thank you for registering with city energy. Please confirm your email by clicking on the following link</p>
+                <a href=http://${addr}:${port}/confirm/?confirmationCode=${confirmationCode}>Confirm Account</a>
+                </div>`,
+          }).catch(err => console.log(err));
+    }  
   };
