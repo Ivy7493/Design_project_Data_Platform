d7f12c0b7fd87376d33e8f86fb001320bfe50adf
Ferreira-VN <1834038@students.wits.ac.za>
2022-08-04 16:57:57 +0200

    Calculate average speed

--- scripts/calculationFunctions.js
+++ scripts/calculationFunctions.js
@@ -2,6 +2,20 @@ let DB = require('./databaseFunctions')
 async function calculateSpeedAverage(){
     let result=await DB.returnSpeedData()
     console.log(result)
+    var sum = 0;
+     result.forEach(function(num) { 
+        let temp=parseFloat(num)
+        sum += temp
+        temp=0; 
+        sum += temp
+        });
+    // for(let i=0;i<result.length;i++){
+    //     let temp=parseFloat(result[i])
+    //     sum += temp
+    //     temp=0;
+    //     console.log("    ",sum)
+    // }
+    let average = sum /result.length;
+    console.log(average)
 }
-
 module.exports={calculateSpeedAverage};
