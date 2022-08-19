5a368ed044fb598ed2d2926a767422fc8a2ad79b
Ferreira-VN <1834038@students.wits.ac.za>
2022-08-19 09:54:35 +0200

    Use elevation API to calc energy

--- scripts/calculationFunctions.js
+++ scripts/calculationFunctions.js
@@ -1,4 +1,6 @@
 const DB = require('./databaseFunctions')
+const fetch=require('node-fetch')
+
 
 async function calculateSpeedAverage(){
     let result=await DB.returnSpeedData()
@@ -27,7 +29,6 @@ async function returnSpeedForRoute(){
 
 async function returnTimeForRoute(){
     let result = await DB.returnTimeData()
-    console.log('time',result)
     return result;
 }
 
@@ -38,7 +39,6 @@ async function ReturnSpeedTimeForRoute(){
         speed: sped,
         time: _time
     }
-    console.log(temp)
     return temp
 }
 async function calcresultEnergy(){
@@ -50,7 +50,6 @@ async function calcresultEnergy(){
     for(let i=0;i<result.length -2;i++){
         sum += parseFloat (result[i]);
     }
-    console.log('sum',sum)
     return sum
 }
 // calculate energy usage using fuel consumption
@@ -60,7 +59,7 @@ async function calcresultEnergy(){
 //     let resultLat = await DB.returnLatitude() 
 //     let resultLong = await DB.returnLongitude()
 //     let velocity=await DB.returnSpeedData()
-//     let fuelType=await DB.returnFuelTypedData()
+//     let fuelType=await DB.returnFuelTypeData()
 //     // convert data from strings to floats
 //     let finalLat = resultLat.map(x=>{
 //         return parseFloat(x)
@@ -114,7 +113,7 @@ async function calcresultEnergy(){
 // }
 async function calcEnergyUsageKinModel(){
     // import data from databaseFunctions
-    let result = await DB.returnAltitude()
+    let resultAlt = await DB.returnAltitude()
     let resultLat = await DB.returnLatitude() 
     let resultLong = await DB.returnLongitude()
     let velocity=await DB.returnSpeedData()
@@ -123,7 +122,6 @@ async function calcEnergyUsageKinModel(){
     // save start position
     let startLong=resultLong[0] 
     let startLat=resultLat[0] 
-    console.log('start',startLong, startLat)
     //initialise variables
     let totalDistance=[]
     let slope=[]
@@ -132,7 +130,7 @@ async function calcEnergyUsageKinModel(){
     let vehicleForce=0
     let coeffRr = 0.02 // an estimate
     let coeffAdf=0.36 //an estimate
-    
+    let allCarsTripDur=[]
     let timeDiff = 1  //Estimation of what tarnsmission frequency is designed to be(in s)
     let efficiency=0.9
     let brakingEfficiency=0.65
@@ -141,14 +139,13 @@ async function calcEnergyUsageKinModel(){
     let powerOfftake=100 //W (why? = 100)
     let totalEnergy = 0
     let allEnergyPerSecondData=[[]]
-    let timeCount = 0
     let durationTrip = 0
     let brakingEnergy=0
     let deltaVelocity=0
     let fR=0
     let fS=0
     let fA=0
-    let totalEnergyAllCars=0
+    let totalEnergyAllCars=[]
     // convert data from strings to floats
     let finalLat = resultLat.map(x=>{
         return parseFloat(x)
@@ -156,16 +153,16 @@ async function calcEnergyUsageKinModel(){
     let finalLong = resultLong.map(x=>{
         return parseFloat(x)
     })
-    let final = result.map(x=>{
+    let final = resultAlt.map(x=>{
         return parseFloat(x)
     })
     // calculations:
     let radius = 6371; // km
-    for (let k=0; k<mass.length-1;k++){   
-    for(let i=0;i<result.length-1;i++){ //final.length-1;i++){
+    for (let k=0; k<mass.length;k++){   
+        for(let i=0;i<final.length-1;i++){ //final.length-1;i++){
+        
         // compute change in elevation
         changeInElev = calcElevchange(final,i);
-        
         // compute geodesic distance
         
         let hypotDistance = displacement(finalLat,finalLong, changeInElev,radius,i)
@@ -212,64 +209,187 @@ async function calcEnergyUsageKinModel(){
         
         let temp2 = (propEnergy + offtakeEnergy+brakingEnergy)/(3.6 * 10**6)
         totalEnergy += temp2 //(propEnergy + offtakeEnergy)/3.6 * 10**6
-        // just to check all info of specific entries -  for testing purposes
-        // if(i===40){
-        //     console.log('totalforce',totalForce)
-        //     console.log('slope',slope[i])
-        //     console.log('vel',velocity[i]/3.6)
-        //     console.log('hypotdistance',hypotDistance)
-        //     console.log('propenergy',propEnergy)
-        //     console.log('brakingenergy',brakingEnergy)
-        //     console.log('offtake energy',offtakeEnergy)
-        //     console.log('fR',fR)
-        //     console.log('fA',fA)
-        //     console.log('fS',fS)
-        //     console.log('vf',vehicleForce)
-        //     console.log('dspeeddiff',dspeedDiff)
-        //     console.log('energy per second',temp2)
-        //     console.log("deltaVelocity", deltaVelocity)
-        //     console.log('changelev',changeInElev)
-        //     console.log('lateralDistance',lateralDistance)
-        // }
-        //console.log('Energy per second',temp2)
-        //console.log('Hypotdistance',hypotDistance)
         allEnergyPerSecondData[k,i] = temp2 
         lateralDistance=0
         //set vars to 0
         propEnergy = 0
         brakingEnergy = 0
-        // timeCount += timeDiff
-        // propEnergy = 0
-        // deltaVelocity = 0
-        // temp2=0
-        // totalForce=0;
-        // dSpeed=0;
-        // expectDspeed=0;
-        // vehicleForce=0
-        // fA=0
-        // fR=0
-        // fS=0
-        
- 
+        durationTrip++
+    
     }
-   
-    console.log('total Energy final',k,totalEnergy) // will be incorrect now because the data does not contain a full trip
+    allCarsTripDur[k]=durationTrip
+    durationTrip=0 
     totalEnergyAllCars[k]=totalEnergy
-    totalEnergy=0
+    totalEnergy=0 
+    
 
 }
- let driver="DriverA"
- let _time = await returnTimeForRoute()
- let temp = {
-    EnergyPerSecond: allEnergyPerSecondData,
-    TotalEnergy: totalEnergy,
-    Time: _time,
-    Driver: driver
+console.log('Duration of trip for all cars',allCarsTripDur)
+console.log('kinmodel energy',totalEnergyAllCars)
+    let driver="DriverA"
+    let _time = await returnTimeForRoute()
+    let temp = {
+       EnergyPerSecond: allEnergyPerSecondData[0],// allEnergyPerSecondData[0]
+       TotalEnergy: totalEnergy, //totalEnergyAllCars[0]
+       Time: _time,
+       Driver: driver
+    }
+    return temp  
 }
-return temp
- 
+
+// kineticmodel using elevation api
+async function calcEnergyUsageKinModelApiElev(){
+    // import data from databaseFunctions
+    let resultLat = await DB.returnLatitude() 
+    let resultLong = await DB.returnLongitude()
+    let velocity=await DB.returnSpeedData()
+    let mass = await DB.returnCarsMass()
+    let area = await DB.returnCarsArea()
+    //initialise variables
+    let totalDistance=[]
+    let slope=[]
+    let totalForce=0
+    let expectDspeed=0
+    let vehicleForce=0
+    let coeffRr = 0.02 // an estimate
+    let coeffAdf=0.36 //an estimate
+    let allCarsTripDur=[]
+    let timeDiff = 1  //Estimation of what tarnsmission frequency is designed to be(in s)
+    let efficiency=0.9
+    let brakingEfficiency=0.65
+    let propEnergy = 0;
+    let offtakeEnergy=0
+    let powerOfftake=100 //W (why? = 100)
+    let totalEnergy = 0
+    let allEnergyPerSecondData=[[]]
+    let timeCount = 0
+    let durationTrip = 0
+    let brakingEnergy=0
+    let deltaVelocity=0
+    let fR=0
+    let fS=0
+    let fA=0
+    let totalEnergyAllCars=[]
+    // elevation api
+    let getApiElevation=0
+    let resultCoord = await DB.returnCoordinateData() 
+    //console.log('helllooo',resultCoord)
+    let elevApi=[]
+    for(let i=0;i<resultCoord.length;i++){ 
+        const url = 'https://api.open-elevation.com/api/v1/lookup?locations='+resultCoord[i]
+        // Storing response
+        let response = await fetch(url);
+            
+        // Storing data in form of JSON
+        var data = await response.json();
+        let array=[]
+        for(var j in data) {
+            array.push([j,data[j]]);    
+                }
+        for(var k in array[0][1]) {
+            array.push([k,array[0][1][k]]);
+            }
+        getApiElevation= array[1][1].elevation
+                    
+        // Calling the elevation api function
+        elevApi[i]=getApiElevation;
+        getApiElevation=0
+        //console.log('elevApi',i,elevApi[i])
+        }
+    let  finalApi=elevApi //await calcApiElev()
+    
+    // convert data from strings to floats
+    let finalLat = resultLat.map(x=>{
+        return parseFloat(x)
+    })
+    let finalLong = resultLong.map(x=>{
+        return parseFloat(x)
+    })
+    // calculations:
+    let radius = 6371; // km
+    for (let k=0; k<mass.length;k++){   
+        for(let i=0;i<finalApi.length-1;i++){ //final.length-1;i++){
+        
+        // compute change in elevation
        
+        changeInElev=calcElevchange(finalApi,i)
+        // compute geodesic distance
+        
+        let hypotDistance = displacement(finalLat,finalLong, changeInElev,radius,i)
+        if (i===0){
+            totalDistance[i]=0
+        }
+        else{
+            totalDistance[i]=hypotDistance
+        }
+        if (totalDistance!==0 && changeInElev!==0){
+            slope[i]= Math.asin(changeInElev/hypotDistance)
+        }
+        else {
+            slope[i]=0
+        }
+        // if velocity is below 0.3 equal to 0, small velocities are considered negligible
+        if (velocity[i]<0.3){
+            velocity[i] = 0
+        }
+        if (velocity[i+1]<0.3){
+            velocity[i+1] = 0
+        }
+
+        if (velocity[i]!==0){
+        fR=rollingResistanceFriction(mass[k], coeffRr,slope[i],velocity[i]/3.6)
+        fA=AerodynamicDragForce(coeffAdf,area[k],velocity[i]/3.6)
+        fS=RoadSlopeDrag(mass[k],slope[i])
+        }
+
+        deltaVelocity=(velocity[i+1]/3.6) - (velocity[i]/3.6)
+        totalForce= fR + fA + fS
+        expectDspeed=(totalForce * timeDiff) / mass[k]
+        dspeedDiff = deltaVelocity - expectDspeed
+        vehicleForce = mass[k] * dspeedDiff / timeDiff
+        let temp3=vehicleForce * (velocity[i]/3.6) * timeDiff
+        if (temp3>0){
+
+          propEnergy= temp3/efficiency//vehicleForce * velocity[i] * timeDiff / efficiency
+        }
+        else{
+          brakingEnergy= temp3*brakingEfficiency//brakingEfficiency * vehicleForce * velocity[i] * timeDiff  //propEnergy
+        }
+        offtakeEnergy= powerOfftake* timeDiff
+        
+        let temp2 = (propEnergy + offtakeEnergy+brakingEnergy)/(3.6 * 10**6)
+        totalEnergy += temp2 //(propEnergy + offtakeEnergy)/3.6 * 10**6
+        allEnergyPerSecondData[k,i] = temp2 
+        lateralDistance=0
+        //set vars to 0
+        propEnergy = 0
+        brakingEnergy = 0
+        durationTrip++
+    
+    }
+    allCarsTripDur[k]=durationTrip
+   
+    durationTrip=0 
+    totalEnergyAllCars[k]=totalEnergy
+    totalEnergy=0 
+    
+
 }
+console.log('Duration of trip for all cars',allCarsTripDur)
+console.log('kinmodelAPI',totalEnergyAllCars)
+    let driver="DriverA"
+    let _time = await returnTimeForRoute()
+    let temp = {
+       EnergyPerSecond: allEnergyPerSecondData[0],// allEnergyPerSecondData[0]
+       TotalEnergy: totalEnergy, //totalEnergyAllCars[0]
+       Time: _time,
+       Driver: driver
+    }
+    return temp  
+}
+
+
+
 //calculate elevation change
 function calcElevchange(final,i){
     changeInElev = (final[i+1] - final[i]);
@@ -290,6 +410,57 @@ function displacement(finalLat,finalLong,changeInElev,radius,i){
     let hypotDistance = Math.hypot(lateralDistance, changeInElev)
     return hypotDistance
 }
+// // elevation from api eleavtion
+// async function calcApiElev(){
+
+// let getApiElevation=0
+// let resultCoord = await DB.returnCoordinateData() 
+// console.log('helllooo',resultCoord)
+// let elevApi=[]
+//     for(let i=0;i<resultCoord.length-1;i++){ 
+//         const url = 'https://api.open-elevation.com/api/v1/lookup?locations='+resultCoord[i]
+//             // Storing response
+//         let response = await fetch(url);
+	
+//         // Storing data in form of JSON
+//         var data = await response.json();
+//         let array=[]
+//         for(var j in data) {
+//             array.push([j,data[j]]);    
+//         }
+//     for(var k in array[0][1]) {
+//         array.push([k,array[0][1][k]]);
+//     }
+//     getApiElevation= array[1][1].elevation
+            
+//         // Calling the elevation api function
+//         elevApi[i]=getApiElevation;
+//         //console.log('elevApi',i,elevApi[i])
+//     }
+//    return elevApi
+// }
+
+// Defining async function for obtaining elevation api
+// async function getApiElevation(url) {
+	
+//   // Storing response
+//   let response = await fetch(url);
+	
+//   // Storing data in form of JSON
+//   var data = await response.json();
+//   console.log('api',data)
+//   let array=[]
+//   for(var i in data) {
+//     array.push([i,data[i]]);
+// }
+// for(var i in array[0][1]) {
+//   array.push([i,array[0][1][i]]);
+// }
+// //console.log('array2',array[1][1].elevation)
+// return array[1][1].elevation
+// }
+
+
 
 //Functions that calculate three environmental forces acting on the vehicle in (N)
 
@@ -318,4 +489,4 @@ function convertToRad(a){
 return a*Math.PI /180;
 }
 
-module.exports={calculateSpeedAverage, returnSpeedForRoute, returnTimeForRoute, ReturnSpeedTimeForRoute,calcEnergyUsageKinModel, calcresultEnergy};
+module.exports={calculateSpeedAverage, returnSpeedForRoute, returnTimeForRoute, ReturnSpeedTimeForRoute,calcEnergyUsageKinModel,calcEnergyUsageKinModelApiElev,calcresultEnergy};
